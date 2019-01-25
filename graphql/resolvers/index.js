d2fcccdd8e588aeb8c6fc8bfd743cc115c8fc464
leonardomso <leonardomso11@gmail.com>
2019-01-25 14:32:14 -0200

    Types and Resolvers in one single file

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,8 +1,18 @@
-import { mergeResolvers } from "merge-graphql-schemas";
+const resolvers = {
+    Query: {
+        getAuthor: async (parent, args, context, info) => {},
+        getAllAuthors: async (parent, args, context, info) => {},
+        getBook: async (parent, args, context, info) => {},
+        getAllBooks: async (parent, args, context, info) => {}
+    },
+    Mutation: {
+        createAuthor: async (parent, args, context, info) => {},
+        deleteAuthor: async (parent, args, context, info) => {},
+        createBook: async (parent, args, context, info) => {},
+        deleteBook: async (parent, args, context, info) => {}
+    },
+    Book: {},
+    Author: {}
+};
 
-import Author from "./Author/";
-import Book from "./Book/";
-
-const resolvers = [Author, Book];
-
-export default mergeResolvers(resolvers);
+export default resolvers;
